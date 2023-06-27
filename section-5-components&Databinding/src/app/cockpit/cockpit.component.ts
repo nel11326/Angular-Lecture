f7cf0601efab530104c8467de9388dcb32dbe289
nel11326 <lenor11326@hotmail.com>
2023-06-28 02:28:41 +0800

    access the template and DOM with @ViewChild

--- section-5-components&Databinding/src/app/cockpit/cockpit.component.ts
+++ section-5-components&Databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -15,7 +15,8 @@ export class CockpitComponent implements OnInit {
     serverContent: string;
   }>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() {}
 
@@ -24,14 +25,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
